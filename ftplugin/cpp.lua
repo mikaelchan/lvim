781cc7d53f38d5af20fac2c3a6394093bede82f5
Michael Chan <hi@michaelchan.space>
2023-06-02 10:52:13 +0800

    remove ccls

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,18 +1,3 @@
-local ccls_opts = {
-  init_options = {
-    compilationDatabaseDirectory = "build",
-    index = {
-      threads = 0,
-    },
-    clang = {
-      excludeArgs = { "-frounding-math" },
-    },
-  },
-  filetypes = { "c", "cpp", "h", "cc" },
-  offset_encoding = "utf-8",
-}
-require("lspconfig").ccls.setup(ccls_opts)
-
 local clangd_opts = {
   cmd = {
     "clangd",
